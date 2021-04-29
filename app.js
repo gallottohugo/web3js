c844cb2c2da70d9d27c89f5641c09dd8c1dfa6d7
gallottohugo <gallottohugo@gmail.com>
2021-04-29 15:05:09 -0300

    Fix problems

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const Web3 = require("web3")
-const EthereumTx = require("ethereumjs-tx")
+const Web3 = require('web3')
+const EthereumTx = require('ethereumjs-tx').Transaction
 
 const web3 = new Web3("https://ropsten.infura.io/v3/19beaa22507c40a4b0986bc5eebcf14c")
 
@@ -7,20 +7,20 @@ const web3 = new Web3("https://ropsten.infura.io/v3/19beaa22507c40a4b0986bc5eebc
 const address1 = "0xA530c8A7c550486Ca6B96219D775A61e0c3F5BdA";
 const address2 = "0x7A917e1D08329AbeDd29B4b08dd3598EA9a624f2";
 
-const address1Key = new Buffer.from('848f7e3e0ffbce3c0ed878027d9700b4d1fc3fa099ba8bc6a5667fc5aff13519')
-const address2Key = new Buffer.from('704107bedff8bb1c53f7957ead59c8e78eca7a7d4d6bc2f2eb25f7fa2d504afa')
+const address1Key = new Buffer.from('848f7e3e0ffbce3c0ed878027d9700b4d1fc3fa099ba8bc6a5667fc5aff13519', 'hex')
+const address2Key = new Buffer.from('704107bedff8bb1c53f7957ead59c8e78eca7a7d4d6bc2f2eb25f7fa2d504afa', 'hex')
 
 
 //balance address 1
-web3.eth.getBalance(address1, (err, _balance) => {
-	console.log("Balance address1: " + web3.utils.fromWei(_balance, "ether"))
-})
+//web3.eth.getBalance(address1, (err, _balance) => {
+//	console.log("Balance address1: " + web3.utils.fromWei(_balance, "ether"))
+//})
 
 
 //balance address 2
-web3.eth.getBalance(address2, (err, _balance) => {
-	console.log("Balance address2: " + web3.utils.fromWei(_balance, "ether"))
-})
+//web3.eth.getBalance(address2, (err, _balance) => {
+//	console.log("Balance address2: " + web3.utils.fromWei(_balance, "ether"))
+//})
 
 
 //get address trx count & send signed trx 
@@ -36,7 +36,8 @@ web3.eth.getTransactionCount(address2, (err, _txCount) => {
 		//data: '0x7f7465737432000000000000000000000000000000000000000000000000000000600057'
 	}
 
-	const tx = new EthereumTx(rawTx);
+	let tx = new EthereumTx(rawTx, {chain:'ropsten', hardfork: 'petersburg'});
+	
 	tx.sign(address2Key);
 
 	const serializedTx = tx.serialize().toString('hex');
