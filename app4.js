7f31acd1023eb7cb5632e0d1188bce2247235ff6
gallottohugo <gallottohugo@gmail.com>
2021-04-30 14:50:34 -0300

    Create multiples wallets

--- app4.js
+++ app4.js
@@ -1,27 +1,33 @@
 // SPDX-License-Identifier: MIT
 // Create Ethereum wallet with web3
 
-const Web3 = require('web3');
+const Web3 = require('web3')
 const web3 = new Web3()
 
 
 
 const entropy = web3.utils.sha3(Math.random(0, 1000000).toString(16) + web3.utils.randomHex(32))
 console.log(" ")
-console.log('Random text: ' + entropy);
+console.log('Random text: ' + entropy)
 console.log(" ")
-const wallet = web3.eth.accounts.create([entropy]);
+const wallet = web3.eth.accounts.create([entropy])
 console.log("Wallet")
 console.log(wallet)
 console.log(" ")
 
-const privateKeyEncrypted = web3.eth.accounts.encrypt(wallet.privateKey, "MyPassword");
+const privateKeyEncrypted = web3.eth.accounts.encrypt(wallet.privateKey, "MyPassword")
 console.log("PrivateKey encrypted")
 console.log(privateKeyEncrypted)
 console.log(" ")
 
 
-const privateKey = web3.eth.accounts.decrypt(privateKeyEncrypted, "MyPassword");
+const privateKey = web3.eth.accounts.decrypt(privateKeyEncrypted, "MyPassword")
 console.log("privateKey decrypted")
 console.log(privateKey)
+console.log(" ")
 
+//Create multiples wallets
+const fiveWallets = web3.eth.accounts.wallet.create(5, entropy)
+console.log(" ")
+console.log("Five wallets")
+console.log(fiveWallets)
